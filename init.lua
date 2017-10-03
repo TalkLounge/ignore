7fd54d189b3b5162ffe51d210b343891995d2403
TalkLounge <talklounge@yahoo.de>
2017-10-03 12:40:50 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,18 +1,22 @@
 local names = {}
 
-minetest.register_chatcommand("ignore",{
-     func = function(param)
-               minetest.run_server_chatcommand("msg",param.." I have put you on my ignore list. I will only receive PMs from you")
-	       names[tostring(param)] = true
-             end})
+minetest.register_chatcommand("ignore", {
+    func = function(param)
+      minetest.run_server_chatcommand("msg", param .." I have put you on my ignore list. I will only receive PMs from you")
+      names[tostring(param)] = true
+    end})
 
-minetest.register_chatcommand("unignore",{
-     func = function(param)
-               minetest.run_server_chatcommand("msg",param.." You are no longer on my ignore list")
-	       names[tostring(param)] = false
-	     end})
+minetest.register_chatcommand("unignore", {
+    func = function(param)
+      minetest.run_server_chatcommand("msg", param .." You are no longer on my ignore list")
+      names[tostring(param)] = false
+    end})
 
 minetest.register_on_receiving_chat_messages(function(msg)
-      return names[string.sub(string.split(msg, ">")[1],2)]
-end)
+    local name = string.split(msg, ">")[1]
+    name = string.sub("test".. name, 6)
+    if name ~= nil then
+      return names[name]
+    end
+  end)
 
